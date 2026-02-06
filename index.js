27d55d6dde3e8f9a2fd7240c50b9b0838de5e820
Julie Rindone <julie.rindone@gmail.com>
2026-02-05 19:19:59 -0800

    Added default image for thumbnails, resetAll(), separate handling for exact vs. fuzzy matches

--- index.js
+++ index.js
@@ -17,14 +17,20 @@ searchBar.addEventListener('blur', () => {
 searchForm.addEventListener('submit', async (e) => {
   e.preventDefault()
 
-  const searchData = Object.fromEntries(new FormData(searchForm))
-
-  // TODO: REMOVE hardcoded searchType
-  const searchType = 't'
-
-  const titleToSearch = (searchData.title).replaceAll(' ', '+')
+  // TODO: Might be better to use a different method to get this ONE field.
   const key = 'aad30e17'
-  const response = await fetch(`https://omdbapi.com/?${searchType}=${titleToSearch}&apikey=${key}`)
+  const searchData = Object.fromEntries(new FormData(searchForm))
+  const titleToSearch = (searchData.title).replaceAll(' ', '+')
+  let typeOfSearch = ''
+  const typeNames = document.getElementsByName('type')
+  for (let type of typeNames) {
+    if (type.checked) {
+      typeOfSearch = type.dataset.letter
+      break
+    }
+  }
+
+  const response = await fetch(`https://omdbapi.com/?${typeOfSearch}=${titleToSearch}&apikey=${key}`)
   const data = await response.json()
 
   if (data.Response !== "True") {
@@ -33,116 +39,101 @@ searchForm.addEventListener('submit', async (e) => {
   }
 
   else {
-    if (searchType === 't') {
+    resetAll()
+
+    if (typeOfSearch === 't') {
       let exactResult = getExactResult(data)
       generateExactResultHtml(exactResult)
     }
     else {
-      let results = getFuzzyResults(data)
-      // console.log(results)
-      // generateHtml(resultArray)
+      getFuzzyResults(data)
+      generateFuzzyResultsHtml(resultArray)
     }
   }
 })
 
 function getExactResult(data) {
+  let thumbnail = getThumbnail(data)
+  let rating = data.Ratings[1] ? data.Ratings[1].Value : "???"
+
   return {
     title: `${data.Title}`,
-    rating: `${data.Ratings[1].Value}`,
+    rating: `${rating}`,
     runtime: `${data.Runtime}`,
     year: `${data.Year}`,
     genre: `${data.Genre}`,
     watchlist: false,
     plot: `${data.Plot}`,
-    thumbnail: `${data.Poster}`,
+    thumbnail: `${thumbnail}`,
     alt: `poster for ${data.Title}`
   }
 }
 
+// TODO: Limit to 10 entries and add "load more movies" button at bottom
 function getFuzzyResults(data) {
   data.Search.forEach(currentMovie => {
     if (!(currentMovie.Title.toLowerCase().includes("commentary"))) {
+      let thumbnail = getThumbnail(currentMovie)
+
       const movie = {
         title: `${currentMovie.Title}`,
         year: `${currentMovie.Year}`,
         watchlist: false,
-        thumbnail: `${currentMovie.Poster}`,
+        thumbnail: `${thumbnail}`,
         alt: `poster for ${currentMovie.Title}`
       }
 
       resultArray.push(movie)
     }
   });
-  // console.log(resultArray)
 }
 
 function generateExactResultHtml(exactResult) {
   let movie = exactResult
 
-  movieCardsWrapper.innerHTML =
-    `<article class="movie-card">
-          <img class="thumbnail" src="${movie.thumbnail}" alt="${movie.alt}">
-          <div class="movie-details">
-            <div class="title-rating">
-              <h2>${movie.title}</h2>
-              <p><i class="fa-solid fa-star"></i>${movie.rating}</p>
-            </div>
-            <div class="runtime-year-list">
-              <div>
-                <p>${movie.runtime}</p>
-                <p>${movie.year}</p>
-              </div>
-              <p class="watchlist">Watchlist <i class="fa-solid fa-circle-plus"></i></p>
-            </div>
-            <div class="genre">
-              <p>${movie.genre}</p>
-            </div>
-            <p class="plot">${movie.plot}</p>
+  // NOTE: p.watchlist class prob doesn't need to exist but I don't feel confident in changing it right now
+  movieCardsWrapper.innerHTML = `
+    <article class="movie-card">
+      <img class="thumbnail" src="${movie.thumbnail}" alt="${movie.alt}">
+      <div class="movie-details">
+        <div class="title-watchlist">
+          <h2>${movie.title}</h2>
+          <p class="watchlist"><i class="fa-solid fa-circle-plus"></i></p>
+        </div>
+        <div class="runtime-year-genre-rating">
+          <div class="runtime-year-genre">
+            <p class="runtime-year">${movie.year}&ensp;${movie.runtime}</p>
+            <p class="genre">${movie.genre}</p>
           </div>
-        </article>
-        <hr class="card-divider">`
+          <p class="rating">
+            <i class="fa-solid fa-star"></i>
+            ${movie.rating}
+          </p>
+        </div>
+        <p class="plot">${movie.plot}</p>
+      </div>
+    </article>
+    <hr class="card-divider">`
 
   movieCardsWrapper.classList.replace('space-saver', 'cards')
-  movieCardsWrapper.innerHTML = result
 }
 
 function generateFuzzyResultsHtml(resultArray) {
-  resultArray.forEach(movie => {
-    // movieCardsWrapper.innerHTML =
-    //   `<article class="movie-card">
-    //       <img class="thumbnail" src="${movie.thumbnail}" alt="${movie.alt}">
-    //       <div class="movie-details">
-    //         <div class="title-rating">
-    //           <h2>${movie.title}</h2>
-    //           <p><i class="fa-solid fa-star"></i>${movie.rating}</p>
-    //         </div>
-    //         <div class="runtime-year-list">
-    //           <div>
-    //             <p>${movie.runtime}</p>
-    //             <p>${movie.year}</p>
-    //           </div>
-    //           <p class="watchlist">Watchlist <i class="fa-solid fa-circle-plus"></i></p>
-    //         </div>
-    //         <div class="genre">
-    //           <p>${movie.genre}</p>
-    //         </div>
-    //         <p class="plot">${movie.plot}</p>
-    //       </div>
-    //     </article>
-    //     <hr class="card-divider">`
-    // })
 
+  resultArray.forEach(movie => {
     allMovieCards +=
-      `<article class="movie-card">
+      `<article class="movie-card fuzzy-results">
         <img class="thumbnail" src="${movie.thumbnail}" alt="${movie.alt}">
         <div class="movie-details">
-          <div class="title-rating">
+          <div class="title-watchlist">
             <h2>${movie.title}</h2>
-            <p>${movie.year}</p>
+            <p class="watchlist"><i class="fa-solid fa-circle-plus"></i></p>
           </div>
-          <div class="runtime-year-list">
-            <p class="watchlist">Watchlist <i class="fa-solid fa-circle-plus"></i></p>
-          </div>
+          <p>${movie.year}</p>
+          <details id="more">
+            <summary>more</summary>
+            <p>The other details will go here.</p>
+          </details>
         </div>
       </article>
       <hr class="card-divider">`
@@ -150,6 +141,19 @@ function generateFuzzyResultsHtml(resultArray) {
 
   movieCardsWrapper.classList.replace('space-saver', 'cards')
   movieCardsWrapper.innerHTML = allMovieCards
+}
+
+function getThumbnail(data) {
+  if ((data.Poster.toLowerCase() === "n/a") || (!data.Poster)) {
+    return "assets/images/film_icon.png"
+  }
+  else {
+    return data.Poster
+  }
+}
+
+function getDetails() {
+  // TODO: Onclick of details, fetch data from the exact match version of the movie selected.
 }
 
 function resetAll(message = null) {
@@ -164,32 +168,3 @@ function resetAll(message = null) {
   			<p id="no-result-message">${message}</p>
 				<i class="fa-solid fa-film"></i>`
 }
-
-
-// FOR TESTING
-// const movie = {
-//   title: `Harold and Maude`,
-//   rating: `10.0`,
-//   runtime: `90 min`,
-//   year: `1971`,
-//   genre: ['comedy', 'romance', 'coming-of-age'],
-//   watchlist: false,
-//   plot: `Young, rich, and obsessed with death, Harold finds himself changed forever when he meets lively septuagenarian Maude at a funeral`,
-//   // plot: `Young, rich, and obsessed with death, Harold finds himself changed forever`,
-//   thumbnail: `assets/images/harold_and_maude.jpg`,
-// }
-
-// // SO not using this constructor.
-// class Movie {
-//   constructor() {
-//     this.title = title
-//     this.rating = rating
-//     this.runtime = runtime
-//     this.year = year
-//     this.genre = genre
-//     this.watchlist = watchlist
-//     this.plot = plot
-//     this.thumbnail = thumbnail
-//     this.alt = alt
-//   }
-// }
